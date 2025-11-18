d517bf8b5b5b951a8417123747cd77af828b3b08
Jinal Khona <jinal.khona01@gmail.com>
2025-11-19 00:08:46 +0100

    Add files via upload

---
+++ scripts/silver/proc_load_silver.sql
@@ -0,0 +1,234 @@
+/*
+===============================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    CALL silver.load_silver();
+===============================================================================
+*/
+
+---bronze.crm_cust_info Table
+--Truncate and Insert transformed\cleaned data for bronze.crm_cust_info into silver layer
+CREATE OR REPLACE PROCEDURE silver.load_silver()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    start_time TIMESTAMP;
+    end_time TIMESTAMP;
+    batch_start_time TIMESTAMP;
+    batch_end_time TIMESTAMP;
+BEGIN
+    batch_start_time := clock_timestamp();
+    RAISE NOTICE '================================================';
+    RAISE NOTICE 'Loading Silver Layer';
+    RAISE NOTICE '================================================';
+
+    RAISE NOTICE '------------------------------------------------';
+    RAISE NOTICE 'Loading CRM Tables';
+    RAISE NOTICE '------------------------------------------------';
+
+    start_time := clock_timestamp();
+RAISE NOTICE '>> Truncating Table: silver.crm_cust_info';
+TRUNCATE TABLE silver.crm_cust_info;
+RAISE NOTICE '>> Inserting Data Into: silver.crm_cust_info';
+INSERT INTO silver.crm_cust_info (cst_id,
+cst_key,
+cst_firstname,
+cst_lastname ,
+cst_marital_status,
+cst_gndr,
+cst_create_date)
+SELECT cst_id ,
+cst_key ,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+     WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+     ELSE 'unknown'
+END AS cst_marital_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+     WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+     ELSE 'unknown'
+     END cst_gndr,
+cst_create_date
+FROM(SELECT *,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+FROM bronze.crm_cust_info WHERE cst_id IS NOT NULL)
+WHERE flag_last = 1;
+
+ end_time := clock_timestamp();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM end_time - start_time);
+	
+---bronze.crm_prd_info Table
+---Insert transformed\cleaned data for bronze.crm_prd_info into silver layer
+start_time := clock_timestamp();
+    RAISE NOTICE '>> Truncating Table: silver.crm_prd_info';
+	TRUNCATE TABLE silver.crm_prd_info;
+	RAISE NOTICE '>> Inserting Data Into: silver.crm_prd_info';
+INSERT INTO silver.crm_prd_info (
+prd_id,
+cat_id,
+prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt
+)
+SELECT
+prd_id,
+REPLACE(SUBSTRING(prd_key,1, 5), '-' , '_')AS cat_id, --Extract category ID
+SUBSTRING(prd_key, 7 , LENGTH(prd_key)) AS prd_key,   --Extract product key
+prd_nm,
+COALESCE(prd_cost, 0) AS prd_cost,
+CASE WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+     WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+	 WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+	 WHEN UPPER(TRIM(prd_line)) = 'T' THEN  'Touring'
+     ELSE 'unknown'
+END AS prd_line,
+CAST(prd_start_dt AS DATE) AS prd_start_dt,
+CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 day' AS DATE) AS prd_end_dt --Calculate end date as one day before the next start date
+FROM bronze.crm_prd_info;
+
+ end_time := clock_timestamp();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM end_time - start_time);
+	
+---bronze.crm_sales_details Table
+---Insert transformed\cleaned data for bronze.crm_sales_details into silver layer
+start_time := clock_timestamp();
+    RAISE NOTICE '>> Truncating Table: silver.crm_sales_details';
+	TRUNCATE TABLE silver.crm_sales_details;
+	 RAISE NOTICE '>> Inserting Data Into: silver.crm_sales_details';
+INSERT INTO silver.crm_sales_details (
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt,
+sls_sales,
+sls_quantity,
+sls_price
+)
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt::text) != 8 THEN NULL 
+      ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+END AS sls_order_dt,
+CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) AS sls_ship_dt,
+CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) AS sls_due_dt,
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR  sls_sales != sls_quantity * ABS(sls_price)
+     THEN sls_quantity * ABS(sls_price)
+	 ELSE sls_sales
+END AS sls_sales,
+sls_quantity,
+CASE WHEN sls_price IS NULL OR sls_price <= 0 
+     THEN sls_sales / NULLIF(sls_quantity,0)
+	 ELSE sls_price
+END AS sls_price
+FROM bronze.crm_sales_details;
+
+end_time := clock_timestamp();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM end_time - start_time);
+
+    RAISE NOTICE '------------------------------------------------';
+    RAISE NOTICE 'Loading ERP Tables';
+    RAISE NOTICE '------------------------------------------------';
+
+---bronze.erp_cust_az12    
+---Insert transformed\cleaned data for bronze.erp_cust_az12 into silver layer
+start_time := clock_timestamp();
+    RAISE NOTICE '>> Truncating Table: silver.erp_cust_az12';
+TRUNCATE TABLE silver.erp_cust_az12;
+RAISE NOTICE '>> Inserting Data Into: bronze.erp_cust_az12';
+INSERT INTO silver.erp_cust_az12(
+cid,
+bdate,gen
+)
+SELECT 
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4,LENGTH(cid)) --Remove 'NAS' prefix if present
+     ELSE cid
+END AS cid,
+CASE WHEN bdate > CURRENT_DATE THEN NULL
+     ELSE bdate
+END AS bdate,    --SET future birthdates to NULL
+CASE WHEN UPPER(TRIM(gen)) IN ('M' , 'MALE') THEN 'Male'
+     WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+     ELSE 'unknown'
+END AS gen  --- Normalize gender values and handle unknown cases
+FROM bronze.erp_cust_az12;
+
+end_time := clock_timestamp();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM end_time - start_time);
+
+   
+---bronze.erp_loc_a101
+---Insert transformed\cleaned data for bronze.erp_loc_a101 into silver layer
+ start_time := clock_timestamp();
+RAISE NOTICE '>> Truncating Table: silver.erp_loc_a101';
+TRUNCATE TABLE silver.erp_loc_a101;
+ RAISE NOTICE '>> Inserting Data Into: silver.erp_loc_a101';
+INSERT INTO silver.erp_loc_a101 (
+cid, cntry
+)
+SELECT 
+REPLACE(cid,'-','') AS cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+     WHEN TRIM(cntry) IN ('US' , 'USA') THEN 'United States'
+	 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'unknown'
+	 ELSE TRIM(cntry)
+END AS cntry
+FROM bronze.erp_loc_a101;
+
+end_time := clock_timestamp();
+RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM end_time - start_time);
+
+    
+---Insert transformed\cleaned data for bronze.erp_px_cat_g1v2 into silver layer
+---bronze.erp_px_cat_g1v2 table
+start_time := clock_timestamp();
+RAISE NOTICE '>> Truncating Table: silver.erp_px_cat_g1v2';
+TRUNCATE TABLE silver.erp_px_cat_g1v2;
+RAISE NOTICE '>> Inserting Data Into: silver.erp_px_cat_g1v2';
+INSERT INTO silver.erp_px_cat_g1v2 (id,cat,subcat, maintenance)
+SELECT 
+id,
+cat,
+subcat,
+maintenance
+FROM bronze.erp_px_cat_g1v2;
+
+ end_time := clock_timestamp();
+    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(SECOND FROM end_time - start_time);
+
+    batch_end_time := clock_timestamp();
+    RAISE NOTICE '==========================================';
+    RAISE NOTICE 'Loading Silver Layer is Completed';
+    RAISE NOTICE '   - Total Load Duration: % seconds', EXTRACT(SECOND FROM batch_end_time - batch_start_time);
+    RAISE NOTICE '==========================================';
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE WARNING '==========================================';
+        RAISE WARNING 'ERROR OCCURRED DURING LOADING SILVER LAYER';
+        RAISE WARNING 'Error Message: %', SQLERRM;
+        RAISE WARNING '==========================================';
+END;
+$$;
+
+--- execute the loading ----
+CALL silver.load_silver();
